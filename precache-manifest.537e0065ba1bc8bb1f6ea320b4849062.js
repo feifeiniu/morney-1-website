7bc80b6f0c7b8ccf24a40e24c307f21a735518af
jingfei <987003803@qq.com>
2021-09-28 12:48:11 +0800

    init

---
+++ precache-manifest.537e0065ba1bc8bb1f6ea320b4849062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cd4349820efc4bc917",
+    "url": "/morney-1-website/css/app.c3469c0c.css"
+  },
+  {
+    "revision": "99afa1258ca65451da0bde799ac1edad",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "80cd4349820efc4bc917",
+    "url": "/morney-1-website/js/app.e46e3e5d.js"
+  },
+  {
+    "revision": "6b0d1566449a18c4af3a",
+    "url": "/morney-1-website/js/chunk-vendors.e47ee4f7.js"
+  },
+  {
+    "revision": "6576646f5c9ff69f4a17734836f662b5",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
